ff64bc19e16bbf5bf330db632efc58b0c191387f
Luis Mazariego <mazariego2011@gmail.com>
2021-02-16 21:24:25 -0600

    environment var

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Hero } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>('http://localhost:3000/heroes');
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroById(id: number): Observable<Hero>{
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`);
   }
 }
